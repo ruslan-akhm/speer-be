a9f3a925c44cbf65f161998cf8ba68a1ca73bc01
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-03-07 22:56:27 -0500

    added router and controller skeleton

---
+++ controllers/authController.js
@@ -0,0 +1,5 @@
+exports.register = async (req, res, next) => {};
+
+exports.login = async (req, res, next) => {};
+
+exports.logout = async (req, res, next) => {};
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-router.post("/signup", (req, res) => {});
+const authController = require("../controllers/authController");
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
+
+router.use(verifyToken);
 
 module.exports = router;
