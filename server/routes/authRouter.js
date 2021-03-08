cf9102b34d227406265409f454cf149728299386
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-03-07 23:02:45 -0500

    upd hierarchy cont.

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -7,6 +7,4 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 
-router.use(verifyToken);
-
 module.exports = router;
